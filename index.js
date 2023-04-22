2613e7e6f41eeba55f73c1b1e4c8bb6f925473ae
Snekxs <creepergang321@gmail.com>
2023-04-22 14:28:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const app = express();
 app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'))
+let visitCount = 0;
 
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: join(__dirname, 'public')});
 })
+
+app.get('api/visits', (req, res) => {
+    visitCount++;
+    const message = { visits: visitCount };
+    res.json(message);
+});
 
 
 app.get('/api/hello', (req, res) => {
